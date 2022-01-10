875e94921060200b505572858601f9248405e062
Syed Nawas Ahamed <syednawasahamed@outlook.com>
2022-01-10 15:25:37 +0530

    Model Trainee added

---
+++ MDUSampleWebAPI/MDUSampleWebAPI/Models/Trainee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//Added...
+using System.ComponentModel.DataAnnotations;
+
+namespace MDUSampleWebAPI.Models
+{
+    public class Trainee
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Technology { get; set; }
+    }
+}
